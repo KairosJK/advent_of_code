4a9f94a18629335f8f4143ee2669d448e85378db
KairosJK <jonathan.kocevar@gmail.com>
2022-11-26 08:06:18 -0500

    [2015] completed day03

--- aoc2015/src/day03/day03.rs
+++ aoc2015/src/day03/day03.rs
@@ -5,8 +5,9 @@ fn add_tuples(tuple1: &mut(i32, i32), tuple2: (i32, i32)) {
     tuple1.1 += tuple2.1;
 }
 
-fn problem_one(input: &String) -> i32 {
+fn problem_one(input: &String) -> usize {
     let mut house_hash: HashMap<(i32, i32),i32>= HashMap::new();
+    house_hash.insert((0, 0), 1);
     let mut current_location: (i32, i32) = (0, 0);
     for char in input.chars() {
         match char {
@@ -16,18 +17,37 @@ fn problem_one(input: &String) -> i32 {
             'v' => add_tuples(&mut(current_location), (0, -1)),
             _   => (),
         }
-        let house_count = house_hash.entry(current_location).or_insert(1);
-        *house_count += 1;
+        *house_hash.entry(current_location).or_insert(0) += 1;
     }
-    return 0;
+    return house_hash.values().filter(|&&x| x >= 1).count();
 }
 
-/* fn problem_two() {
-
-} */
+fn problem_two(input: &String) -> usize {
+    let mut house_hash: HashMap<(i32, i32),i32>= HashMap::new();
+    house_hash.insert((0, 0), 2);
+    let mut santa_location: (i32, i32) = (0, 0);
+    let mut robo_santa_location: (i32, i32) = (0, 0);
+    for (i, char) in input.chars().enumerate() {
+        let current_location: &mut(i32, i32);
+        if i  % 2 == 0 {
+            current_location = &mut(santa_location);
+        } else {
+            current_location = &mut(robo_santa_location);
+        }
+        match char {
+            '>' => add_tuples(current_location, (1, 0)),
+            '<' => add_tuples(current_location, (-1, 0)),
+            '^' => add_tuples(current_location, (0, 1)),
+            'v' => add_tuples(current_location, (0, -1)),
+            _   => (),
+        }
+        *house_hash.entry(*current_location).or_insert(0) += 1;
+    }
+    return house_hash.values().filter(|&&x| x >= 1).count();
+}
 
 fn main() {
     let input_data = read_to_string("input/day03.input").unwrap();
     println!("Problem 1: {}", problem_one(&input_data));
-   /* println!("Problem 2: {}", problem_two(input_data)); */
+    println!("Problem 2: {}", problem_two(&input_data));
 }
