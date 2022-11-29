01038ccdfbe6d4a379aa11d755e428a0fd29ac05
KairosJK <jonathan.kocevar@gmail.com>
2022-11-29 10:00:15 -0500

    [2015] completed question 1 day06

--- aoc2015/src/day06/day06.rs
+++ aoc2015/src/day06/day06.rs
@@ -5,18 +5,7 @@ fn problem_one(input: &String) -> usize {
     for current_line in input.lines() {
         mutate_lights(current_line, &mut light_field)
     }
-    for i in light_field {
-        for x in i {
-            match x {
-                true => print!("1 "),
-                false => print!("0 "),
-                _ => (),
-            }
-        }
-        print!("\n");
-    }
-    return 0
-    //light_field.iter().flatten().filter(|x| **x).count()
+    light_field.iter().flatten().filter(|x| **x).count()
 }
 
 /* fn problem_two(input: &String) -> i32 {
@@ -29,7 +18,7 @@ fn mutate_lights(line: &str, light_field: &mut Vec<Vec<bool>>) {
                                 .filter(|x| x.chars().all(|c| c.is_ascii_digit()))
                                 .map(|digit| digit.parse::<usize>().unwrap())
                                 .collect();   
-     if line.contains("toggle") {
+    if line.contains("toggle") {
         light_negate(coord_vector, light_field);
     } else if line.contains("turn on") {
         light_on(coord_vector, light_field);
@@ -39,31 +28,31 @@ fn mutate_lights(line: &str, light_field: &mut Vec<Vec<bool>>) {
 }
 
 fn light_negate(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..coord_vector[2] {
-        for y in coord_vector[1]..coord_vector[3] {
+    for x in coord_vector[0]..=coord_vector[2] {
+        for y in coord_vector[1]..=coord_vector[3] {
             light_field[x][y] = !light_field[x][y];
         }
     }
 }
 
 fn light_on(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..coord_vector[2] {
-        for y in coord_vector[1]..coord_vector[3] {
+    for x in coord_vector[0]..=coord_vector[2] {
+        for y in coord_vector[1]..=coord_vector[3] {
             light_field[x][y] = true;
         }
     }
 }
 
 fn light_off(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..coord_vector[2] {
-        for y in coord_vector[1]..coord_vector[3] {
+    for x in coord_vector[0]..=coord_vector[2] {
+        for y in coord_vector[1]..=coord_vector[3] {
             light_field[x][y] = false;
         }
     }
 }
 
 fn main() {
-    let input_data = read_to_string("input/day0601.input").unwrap();
+    let input_data = read_to_string("input/day06.input").unwrap();
     println!("Problem 1: {}", problem_one(&input_data));
     /* println!("Problem 2: {}", problem_two(&input_data)); */
 }
