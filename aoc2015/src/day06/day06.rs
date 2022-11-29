98c629c68f9f30f90ef62badb57284c8fa422272
KairosJK <jonathan.kocevar@gmail.com>
2022-11-29 10:08:44 -0500

    [2015] completed day06

--- aoc2015/src/day06/day06.rs
+++ aoc2015/src/day06/day06.rs
@@ -3,50 +3,71 @@ use std::fs::read_to_string;
 fn problem_one(input: &String) -> usize {
     let mut light_field = vec![vec![false; 1000]; 1000];
     for current_line in input.lines() {
-        mutate_lights(current_line, &mut light_field)
+        q1_mutate_lights(current_line, &mut light_field)
     }
     light_field.iter().flatten().filter(|x| **x).count()
 }
 
-/* fn problem_two(input: &String) -> i32 {
-    return 0;
-} */
+fn problem_two(input: &String) -> usize {
+    let mut light_field = vec![vec![0; 1000]; 1000];
+    for current_line in input.lines() {
+        q2_mutate_lights(current_line, &mut light_field)
+    }
+    light_field.iter().flatten().sum()
+}
 
-fn mutate_lights(line: &str, light_field: &mut Vec<Vec<bool>>) {
+fn q1_mutate_lights(line: &str, light_field: &mut Vec<Vec<bool>>) {
     let delim_str: Vec<&str> = line.split(|c| c == ' ' || c == ',').collect();
     let coord_vector: Vec<usize> = delim_str.iter()
                                 .filter(|x| x.chars().all(|c| c.is_ascii_digit()))
                                 .map(|digit| digit.parse::<usize>().unwrap())
                                 .collect();   
     if line.contains("toggle") {
-        light_negate(coord_vector, light_field);
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                light_field[x][y] = !light_field[x][y];
+            }
+        }
     } else if line.contains("turn on") {
-        light_on(coord_vector, light_field);
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                light_field[x][y] = true;
+            }
+        }
     } else if line.contains("turn off") {
-        light_off(coord_vector, light_field);
-    }
-}
-
-fn light_negate(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..=coord_vector[2] {
-        for y in coord_vector[1]..=coord_vector[3] {
-            light_field[x][y] = !light_field[x][y];
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                light_field[x][y] = false;
+            }
         }
     }
 }
 
-fn light_on(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..=coord_vector[2] {
-        for y in coord_vector[1]..=coord_vector[3] {
-            light_field[x][y] = true;
+fn q2_mutate_lights(line: &str, light_field: &mut Vec<Vec<usize>>) {
+    let delim_str: Vec<&str> = line.split(|c| c == ' ' || c == ',').collect();
+    let coord_vector: Vec<usize> = delim_str.iter()
+                                .filter(|x| x.chars().all(|c| c.is_ascii_digit()))
+                                .map(|digit| digit.parse::<usize>().unwrap())
+                                .collect();   
+    if line.contains("toggle") {
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                light_field[x][y] += 2;
+            }
         }
-    }
-}
-
-fn light_off(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
-    for x in coord_vector[0]..=coord_vector[2] {
-        for y in coord_vector[1]..=coord_vector[3] {
-            light_field[x][y] = false;
+    } else if line.contains("turn on") {
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                light_field[x][y] += 1;
+            }
+        }
+    } else if line.contains("turn off") {
+        for x in coord_vector[0]..=coord_vector[2] {
+            for y in coord_vector[1]..=coord_vector[3] {
+                if light_field[x][y] != 0 {
+                    light_field[x][y] -= 1;
+                }
+            }
         }
     }
 }
@@ -54,5 +75,5 @@ fn light_off(coord_vector: Vec<usize>, light_field: &mut Vec<Vec<bool>>) {
 fn main() {
     let input_data = read_to_string("input/day06.input").unwrap();
     println!("Problem 1: {}", problem_one(&input_data));
-    /* println!("Problem 2: {}", problem_two(&input_data)); */
+    println!("Problem 2: {}", problem_two(&input_data));
 }
