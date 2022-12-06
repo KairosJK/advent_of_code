c700d5f8c9a046c95c7b994a03c55ab041695010
KairosJK <jonathan.kocevar@gmail.com>
2022-12-06 16:36:35 -0500

    [2022] refactored day06

--- aoc2022/src/day06/day06.rs
+++ aoc2022/src/day06/day06.rs
@@ -1,45 +1,27 @@
 use std::fs::read_to_string;
+use itertools::Itertools;
 
-fn problem_one(input: &String) -> usize {
-    let vec: Vec<u8> = input.chars().map(|c| c as u8).collect();
-    for i in 0..vec.len()-4 {
-        let comb_vec = vec![vec[i], vec[i+1], vec[i+2], vec[i+3]];
-        if get_c_a(comb_vec) {
-            return i+4;
+fn universal_solve(input: &String, delim: usize) -> usize {
+    let string_bytes: Vec<u8> = input.chars().map(|c| c as u8).collect();
+    for i in 0..string_bytes.len()-delim {
+        let substring = string_bytes[i..i+delim].to_vec();
+        if  substring.iter().unique().collect::<Vec<&u8>>().len() == substring.len() {
+            return i+delim;
         }
     }
-    return input.len();
+    input.len()
 }
 
-fn get_c_a(comb_vec: Vec<u8>) -> bool {
-    let length = comb_vec.len();
-    for x in 0..length {
-        for y in 0..length {
-            if comb_vec[x] == comb_vec[y] && x != y {
-                return false;
-            }
-        }
-    }
-    true
+fn problem_one(input: &String) -> usize {
+    universal_solve(input, 4)
 }
 
 fn problem_two(input: &String) -> usize {
-    let vec: Vec<u8> = input.chars().map(|c| c as u8).collect();
-    for i in 0..vec.len()-13 {
-        let comb_vec = vec[i..i+14].to_vec();
-/*         for x in comb_vec.clone() {
-            print!("{} ", x as char);
-        }
-        println!("\n\n"); */
-        if get_c_a(comb_vec) {
-            return i+14;
-        }
-    }
-    return input.len();
+    universal_solve(input, 14)
 }
 
 fn main() {
     let input_data = read_to_string("input/day06.input").unwrap();
-    println!("Problem 1: {}", problem_one(&input_data));
-    println!("Problem 2: {}", problem_two(&input_data));
+    println!("Problem 1: {}", problem_one(&input_data)); // 1912
+    println!("Problem 2: {}", problem_two(&input_data)); // 2122
 }
